a89abc2fb6890e7eed8dc8ce7654c96fd0ebd5fb
Arjun Sanyal <arjun@openforce.net>
2002-08-13 19:03:33 +0000

    fix acs_event__recurrence_timespan_edit thanks to deds@infiniteinfo.com

--- sql/postgresql/acs-events-create.sql
+++ sql/postgresql/acs-events-create.sql
@@ -753,22 +753,34 @@ DECLARE
         v_one_end_date               timestamp;
 BEGIN
         -- get the initial offsets
-        select start_date, end_date into v_one_start_date, v_one_end_date
-        from time_intervals, timespans, acs_events
-        where time_intervals.interval_id = timespans.interval_id and
-        timespans.timespan_id = acs_events.timespan_id and
-        event_id= recurrence_timespan_edit.event_id;
+        select start_date,
+               end_date into v_one_start_date,
+               v_one_end_date
+        from time_intervals, 
+             timespans, 
+             acs_events 
+        where time_intervals.interval_id = timespans.interval_id
+          and timespans.timespan_id = acs_events.timespan_id
+          and event_id=p_event_id;
 
-        FOR v_timespan in 
-        (select * from time_intervals where interval_id in (select interval_id from timespans where timespan_id in (select timespan_id from acs_events where recurrence_id = (select recurrence_id from acs_events where event_id = recurrence_timespan_edit.event_id))))
+        FOR v_timespan in
+            select *
+            from time_intervals
+            where interval_id in (select interval_id
+                                  from timespans 
+                                  where timespan_id in (select timespan_id
+                                                        from acs_events 
+                                                        where recurrence_id = (select recurrence_id 
+                                                                               from acs_events where event_id = p_event_id)))
         LOOP
-                PERFORM time_interval__edit(v_timespan.interval_id, v_timespan.start_date + (p_start_date - v_one_start_date), v_timespan.end_date + (p_end_date - v_one_end_date));
+                PERFORM time_interval__edit(v_timespan.interval_id, 
+                                            v_timespan.start_date + (p_start_date - v_one_start_date), 
+                                            v_timespan.end_date + (p_end_date - v_one_end_date));
         END LOOP;
 
-        return p_recurrence_id;
+        return p_event_id;
 END;
 ' language 'plpgsql';
-
 
 create function acs_event__activity_set (
        --
