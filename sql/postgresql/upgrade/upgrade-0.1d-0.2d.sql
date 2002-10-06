eb175945ac417519f0e16083400c349bc4d3d668
Roberto Mello <rmello@fslc.usu.edu>
2002-10-06 23:43:39 +0000

    - Converted inneficient query to INNER JOINs. - Added Oracle upgrade script.

--- sql/postgresql/upgrade/upgrade-0.1d-0.2d.sql
+++ sql/postgresql/upgrade/upgrade-0.1d-0.2d.sql
@@ -1,9 +1,7 @@
 -- created by arjun@openforce.net 
 -- from a bug fix by  Deds Castillo
 
-drop function acs_event__recurrence_timespan_edit;
-
-create function acs_event__recurrence_timespan_edit (
+create or replace function acs_event__recurrence_timespan_edit (
        integer,
        timestamp,
        timestamp
@@ -27,15 +25,24 @@ BEGIN
           and timespans.timespan_id = acs_events.timespan_id
           and event_id=p_event_id;
 
+        -- RBM: Converted inneficient query to INNER JOINs (2002-10-06)
+	--
+        -- FOR v_timespan in
+        --    select *
+        --    from time_intervals
+        --    where interval_id in (select interval_id
+        --                          from timespans 
+        --                          where timespan_id in (select timespan_id
+        --                                                from acs_events 
+        --                                                where recurrence_id = (select recurrence_id 
+        --                                                                       from acs_events where event_id = p_event_id)))
+
         FOR v_timespan in
-            select *
-            from time_intervals
-            where interval_id in (select interval_id
-                                  from timespans 
-                                  where timespan_id in (select timespan_id
-                                                        from acs_events 
-                                                        where recurrence_id = (select recurrence_id 
-                                                                               from acs_events where event_id = p_event_id)))
+            SELECT ti.*
+              FROM time_intervals ti, timespans t, acs_events ae
+	     WHERE ti.interval_id = t.interval_id
+	       AND t.timespan_id = ae.timespan_id
+	       AND ae.event_id = p_event_id
         LOOP
                 PERFORM time_interval__edit(v_timespan.interval_id, 
                                             v_timespan.start_date + (p_start_date - v_one_start_date), 
